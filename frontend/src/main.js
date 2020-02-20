3bad539b7cb92ee468bd77c1a7a527c72ecb8f94
SergioHenrique19 <garciasergiodm@gmail.com>
2020-02-20 14:01:00 -0300

    :rocket: Adiciona config do VueLogger

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import './styles/style.scss';
+import './styles/style.scss'
+import VueLogger from 'vuejs-logger'
 
 Vue.config.productionTip = false
+
+const options = {
+  isEnabled: true,
+  logLevel : 'debug',
+  stringifyArguments : false,
+  showLogLevel : true,
+  showMethodName : false,
+  separator: '|',
+  showConsoleColors: true
+};
+
+Vue.use(VueLogger, options)
 
 new Vue({
   router,
